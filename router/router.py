f3c0493cb8ac08428129e93d6c898464060811c3
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2023-06-07 19:50:03 +0300

    add method check in router.py

--- router/router.py
+++ router/router.py
@@ -2,21 +2,25 @@ from router.prefix_tree import PrefixTree
 
 
 class MakeRouter:
+    ALLOWED_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+
     def __init__(self, routes):
         self.routes_tree = PrefixTree()
         for route in routes:
             self.routes_tree.add_route(route)
 
     def serve(self, request):
-        path = request['path']
-        target_route = self.routes_tree.find_route(path)
+        if request['method'] not in self.ALLOWED_METHODS:
+            raise Exception(f'request method {request["method"]} not allowed')
+        target_route = self.routes_tree.find_route(request)
         if not target_route:
             raise Exception("No route found")
         pattern_path = target_route.get_pattern_path()
         result = {
             'path': pattern_path,
-            'handler': target_route.get_handler(),
-            'params': self.get_route_params(path, pattern_path)
+            'method': request['method'],
+            'handler': target_route.get_handler(request['method']),
+            'params': self.get_route_params(request['path'], pattern_path)
         }
         return result
 
